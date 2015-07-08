45ce5a087260fd2f04182b7385651ccff41458a3
AquaSolid <filip.markoski45@gmail.com>
2015-07-08 16:34:48 +0200

    Show.

--- Interactions/src/interactions/Human.java
+++ Interactions/src/interactions/Human.java
@@ -5,10 +5,55 @@
  */
 package interactions;
 
+import java.util.Random;
+
 /**
  *
  * @author filip
  */
 public class Human {
     
+    String Sex;
+    String Name;
+    String Surname;
+    int Age;
+    int IDnumber;
+    double Money;
+
+    boolean Ambition;
+    int Will;
+    
+    public void Norm ( boolean Conformism, int Homeostasis ) {
+        if (Conformism == false){
+            //Activate Homeostasis Method
+        }
+    }
+    
+    public void Regression (int Homeostasis, int Power) {
+        //Increase Homeostasis's number, lower Power's, do within a cycle 
+        // You will need to add some kind of trial number
+        // Every time someone
+    //    for(int i=0; i<trial.length; i++){
+            //Somekind of Will or Ambition System is need of implementation 
+    //       Homeostasis++;
+    //    }
+    }
+    public void Job (String Name, String Surname, double Money, boolean Ambition, int Will, double Salary){
+        if(Ambition == true && Will<=5 && Will>=0){
+            Will++;
+        }else{
+            Will--;
+        }
+        Money = Money + Salary;
+    }
+    
+    public void Riot (int Age, int IDnumber){
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(100);
+        if(randomInt > 51){
+            //Affect Government
+        }else{
+            //Suffer Punishment
+        }
+    }
 }
