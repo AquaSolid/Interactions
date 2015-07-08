bb1d7170292658353ecc3e99ce76515933f52012
AquaSolid <filip.markoski45@gmail.com>
2015-07-08 16:25:26 +0200

    Test2

---
+++ Interactions/src/interactions/Interactions.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interactions;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author filip
+ */
+public class Interactions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Interactions");
+        
+    }
+    
+}
